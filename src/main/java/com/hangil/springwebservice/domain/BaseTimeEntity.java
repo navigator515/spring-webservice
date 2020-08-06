fd49611580ffa01f80659e43f92b3be9de233a0a
navigator515 <gksrlf515@hanmail.net>
2020-08-06 15:28:35 +0900

    spring boot 간단 API 동작 Test 추가 및 시연

---
+++ src/main/java/com/hangil/springwebservice/domain/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.hangil.springwebservice.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createddate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
